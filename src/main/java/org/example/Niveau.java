eb523b093a031424a1f1293c9db771ccbd75530c
florianbsc <floflo_200068@hotmail.fr>
2024-09-23 17:05:20 +0200

    commence a faire le CRUD du niveau

--- src/main/java/org/example/Niveau.java
+++ src/main/java/org/example/Niveau.java
@@ -9,4 +9,31 @@ public class Niveau {
     public ArrayList<FormeGeo> getFormeGeos() {
         return formeGeos;
     }
+
+    //    CRUD ??
+
+    public void setFormeGeos(ArrayList<FormeGeo> formeGeos) {
+        this.formeGeos = formeGeos;
+    }
+    
+    public void addFormeGeo(FormeGeo formeGeo) {
+        formeGeos.add(formeGeo);
+        
+    }
+    public void editFormeGeo (FormeGeo formeGeo) {
+        int index = formeGeos.indexOf(formeGeo);
+    } 
+    
+    public void removeFormeGeo (FormeGeo formeGeo) {
+        formeGeos.remove(formeGeo);
+    }
+    public double allAire( FormeGeo getAir) {
+        double  totalAire = 0;
+        return totalAire;
+    }
+    
+    public double allPermietre (FormeGeo getPermietre) {
+        double  totalPermietre = 0;
+        return totalPermietre;
+    }
 }
